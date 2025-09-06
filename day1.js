489dc20e55846c4d418d759e95be0545b18ee5c7
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2025-09-06 22:00:44 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,31 @@
+var a=200;
+
+{
+    let a=300
+}
+
+let b=a;
+{
+    let b=-400
+}
+
+console.log(a);
+console.log(b);
+
+//example of every method
+
+// let arr=[8,2,6,4]
+// let result=arr.every(a=> a%2==0)
+// console.log(result);
+
+const arr=[1,2,3,4,4]
+console.log(typeof arr);
+const isarr=Array.isArray(arr)
+console.log(isarr);
+
+const str="asha".toUpperCase
+// str.length
+console.log(str);
+
+console.log(typeof str);
+// console.log(typeof strs);
