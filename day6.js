a2fe97f7bfa81ecebeb89a0fd3e2ba545f7887e1
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2025-09-26 15:58:06 +0530

    day6

---
+++ day6.js
@@ -0,0 +1,60 @@
+one(4)
+
+function one(num){
+    total=num+2
+    // console.log(total);
+    
+}
+
+//one(4)
+
+//two()
+
+const two=function(){
+    sum=2
+}
+//two()
+
+// console.log(username1);
+
+const username1= "astha bhardwaj"
+
+// console.log(username2);
+
+var username2= "Ravita Gautam"
+
+// console.log(username3);
+
+let username3= "Mukul Kushwaha"
+
+let obj1={
+    student_name:"astha",
+    email:"astha@example.com",
+    phone_number:1234
+
+}
+
+const three=(obj1)=>{
+    result=obj1.email
+    console.log(result);
+    
+}
+
+three(obj1)
+
+let obj2={
+    website:"chatgpt",
+    purpose:"study",
+    four(){
+        console.log(this.purpose);
+        
+    }
+}
+obj2.four()
+
+
+
+
+// function(){
+    
+// }
