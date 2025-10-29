306f787a2f696f56a5e3be09e6642e9c55e86a7a
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2025-10-29 18:43:23 +0530

    nullish

--- day17.js
+++ day17.js
@@ -1,2 +1,46 @@
-console.log("astha");
-console.log("astha bhardwaj");
+// function expensive() {
+//   console.log("expensive() called");
+//   return 100;
+// }
+// let val = 5;
+// console.log(expensive() ?? val); // prints 100 (expensive() IS called)
+
+
+let name = null;
+let othername = 'astha';
+name ??= othername;
+console.log(name);
+console.log(othername);
+
+let name1 =null
+let othername2= 'ravita'
+console.log(name1??othername2);
+console.log(name1);
+console.log(othername2);
+
+
+
+// example for react project
+
+
+// function UserCard({ name, age }) {
+//   const displayName = name ?? "Guest";
+//   const displayAge = age ?? "Not Provided";
+
+//   return (
+//     <div>
+//       <h2>{displayName}</h2>
+//       <p>Age: {displayAge}</p>
+//     </div>
+//   );
+// }
+
+// // Usage
+// <UserCard name="" age={null} />
+
+
+
+
+
+
+
---
+++ day18.js
@@ -0,0 +1,19 @@
+// let name=[]
+// let name2= 'astha'
+
+// console.log(name||name2);
+
+
+let name='ravita'
+let name2= null
+let name3= 'astha'
+let name4= 'mukul'
+
+console.log((name||name2)??(name3??name4));
+
+
+// () → Parentheses → Highest priority
+
+// ?? → Nullish coalescing
+
+// || → Logical OR
