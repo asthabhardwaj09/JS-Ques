c677839d877eece07a9419b103b31e5a157d7975
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2025-10-30 14:05:27 +0530

    changes

---
+++ Revision/day21.js
@@ -0,0 +1,23 @@
+// let obj= new Object(
+//     {username:"astha",
+//      id:123
+//     }
+
+// )
+
+// console.log(obj);
+
+
+
+let obj1={
+    Username:'astha',
+    email:'astha@example.com',
+    phoneNumber:1234567890,
+    id:'astha123'
+}
+
+
+console.log(Object.keys(obj1));
+// console.log(Object.entries(obj1));
+
+console.log(obj1.hasOwnProperty("Username"))
